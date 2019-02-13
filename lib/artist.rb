96aa2ad067a368192a8c25c71dc711170d8064bf
Indre Bakeer <indre.bakeer@gmail.com>
2019-02-13 17:50:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,22 @@
 class Artist
   attr_accessor :name
-  @@all = []
-
   def initialize(name)
     @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def new_song(name, genre)
     @songs = []
-    song = Song.new(name, self, genre)
-    self.songs << song.name
+  end
+
+  def add_song(song)
+    @songs << song
     song.artist = self
   end
 
   def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
+    @songs
   end
 
   def genres
-    Song.all.select do |genre|
-      genre.artist == self
+    self.songs.collect do |song|
+      song.genre
     end
   end
-
 end
