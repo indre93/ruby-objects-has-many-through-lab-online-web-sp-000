18e441e20e886b63f023e848d3d864436ec4ef3a
Indre Bakeer <indre.bakeer@gmail.com>
2019-02-13 17:59:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,12 +15,12 @@ class Artist
   def new_song(name, genre)
     song = Song.new(name, self, genre)
     song.artist = self
+    @songs << song.artist
   end
 
   def songs
     Song.all.select do |song|
       song.artist == self
-      @songs << song.artist
     end
   end
 
